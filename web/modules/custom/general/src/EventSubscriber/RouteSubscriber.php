d741667927ffac73737108e876a9ae235b1a403e
raul.altmae <raul.altmae@we.ee>
2022-01-26 16:38:25 +0200

    sample cost sidebar and fixed footer

---
+++ web/modules/custom/general/src/EventSubscriber/RouteSubscriber.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Drupal\general\EventSubscriber;
+
+use Drupal\Core\Routing\RouteSubscriberBase;
+use Drupal\Core\Routing\RoutingEvents;
+use Symfony\Component\Routing\RouteCollection;
+
+/**
+ * general route subscriber.
+ */
+class RouteSubscriber extends RouteSubscriberBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function alterRoutes(RouteCollection $collection) {
+    if ($route = $collection->get('entity.webform_submission.edit_form')) {
+      $route->setPath( '/submissions/webform/manage/{webform}/submission/{webform_submission}/edit');
+      $route->setOption('_admin_route', FALSE);
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    $events = parent::getSubscribedEvents();
+
+    // Use a lower priority than \Drupal\views\EventSubscriber\RouteSubscriber
+    // to ensure the requirement will be added to its routes.
+    $events[RoutingEvents::ALTER] = ['onAlterRoutes', -300];
+
+    return $events;
+  }
+
+}
---
+++ web/modules/custom/sample_cost/src/Plugin/Block/SampleCostBlock.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Drupal\sample_cost\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Datetime\DrupalDateTime;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides an example block.
+ *
+ * @Block(
+ *   id = "sample_cost",
+ *   admin_label = @Translation("Sample Cost"),
+ *   category = @Translation("Sample Cost")
+ * )
+ */
+class SampleCostBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  private EntityTypeManagerInterface $entityTypeManager;
+
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->entityTypeManager = $entity_type_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('entity_type.manager'),
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $webformSubmissions = $this->entityTypeManager->getStorage('webform_submission')->loadByProperties(['webform_id' => 'sample_request']);
+    $currentYear = DrupalDateTime::createFromFormat('Y-m-d', date('Y').'-01-01');
+    $sampleCost = 0;
+    $carrierCost = 0;
+    /** @var \Drupal\webform\WebformSubmissionInterface $webformSubmission */
+    foreach ($webformSubmissions as $webformSubmission) {
+      $webformData = $webformSubmission->getData();
+      $orderDate = DrupalDateTime::createFromFormat('Y-m-d', $webformData['order_date']);
+      $china = strtolower($webformData['china']);
+      if ($orderDate >= $currentYear && $china === 'yes') {
+        $sampleCost += (float) $webformData['total_cost_of_order'];
+        $carrierCost += (float) $webformData['carrier_cost_usd'];
+      }
+    }
+
+    return [
+      'sample_cost' => $sampleCost,
+      'carrier_cost' => $carrierCost,
+      'total' => $sampleCost + $carrierCost,
+    ];
+  }
+
+}
