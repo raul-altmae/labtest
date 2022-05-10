304936a366bedfc97198eb0008a48125ac8cc378
raul.altmae <raul.altmae@we.ee>
2022-05-10 09:38:54 +0300

    submission json feed and config update

---
+++ web/modules/custom/general/src/Controller/GeneralController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Drupal\general\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\taxonomy\Entity\Term;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+/**
+ * Returns responses for general routes.
+ */
+class GeneralController extends ControllerBase {
+  const SAMPLE_REQUEST_TERMS = ['country', 'customer', 'product', 'order_raised_by', 'sample_type', 'size_autocomplete', 'care_label', 'edge_finishing', 'stitch_pattern', 'needle_spacing', 'packaging', 'shipping_method', 'current_status', 'china'];
+
+  /**
+   * Builds the response.
+   */
+  public function feed() {
+    $submissions = $this->entityTypeManager()->getStorage('webform_submission')->loadByProperties(['webform_id' => 'sample_request']);
+    $items = [];
+    /** @var \Drupal\webform\WebformSubmissionInterface $submission */
+    foreach ($submissions as $submission) {
+      $data = $submission->getData();
+      $this->processTerms($data, self::SAMPLE_REQUEST_TERMS);
+      $items[] = $data;
+    }
+    return JsonResponse::create(['items' => $items]);
+  }
+
+  private function processTerms(array &$data, array $termFields) {
+    foreach ($data as $key => $value) {
+      if (in_array($key, $termFields, TRUE)) {
+        $data[$key] = $this->getTermName($data[$key]);
+      }
+    }
+  }
+
+  private function getTermName($id) {
+    $term = Term::load($id);
+    if (!empty($term)) {
+      return $term->getName();
+    }
+    return $id;
+  }
+
+}
