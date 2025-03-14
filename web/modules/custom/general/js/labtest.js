6df8fc76c12b9fda19c1f6dd589cb56ef975755a
Raul Altmäe <raul@begin.ee>
2025-03-14 12:14:21 +0200

    Fix edit form

--- web/modules/custom/general/js/labtest.js
+++ web/modules/custom/general/js/labtest.js
@@ -1,9 +1,13 @@
 (function ($, Drupal, drupalSettings) {
 jQuery(document).ready(() => {
+  const testingContainer = jQuery("#edit-container-testing");
 
   jQuery("#edit-customer").on('change', setTests);
+  if (drupalSettings.editForm) {
+    testingContainer.on('change', addTestFields);
+  }
 
-  jQuery("#edit-container-testing").on('change', setTestValue);
+  testingContainer.on('change', setTestValue);
 
   jQuery("#edit-return-to-sender").on('change', function () {
     const comments = jQuery("#edit-comments")
@@ -25,6 +29,24 @@ ${requestedByName}`;
   });
   setTests();
 });
+
+  function addTestFields() {
+    const tests = jQuery(".testingValues");
+
+   tests.each(function() {
+      const test = jQuery(this).val();
+      const testResults = document.getElementById(`container-${test}-results`)
+      const testPassed =  document.getElementById(`container-${test}-passed`);
+      if (this.checked) {
+        testResults.classList.remove("hidden");
+        testPassed.classList.remove("hidden");
+      } else {
+        testResults.classList.add("hidden");
+        testPassed.classList.add("hidden");
+      }
+    });
+
+  }
 
 function setTestValue() {
   const editTests = jQuery("#edit-tests");
@@ -70,10 +92,9 @@ function setTests() {
           return JSON.parse(item);
         });
         const testValue = testValues.find((value) => value.name === test);
-        if (!testValue) continue;
         testing.append(
-          `<div data-drupal-selector="edit-flexbox-${test}" class="webform-flexbox js-webform-flexbox js-form-wrapper grow justify-between form-wrapper items-center" id="edit-flexbox-${test}"><div class="grow justify-start webform-flex"><div class="webform-flex--container"><div class="form-type-checkbox js-form-item form-item js-form-type-checkbox form-type--checkbox form-type--boolean js-form-item-test form-item--test">
-              <input data-drupal-selector="edit-${test}" type="checkbox" id="edit-${test}" name="${test}" value="${test}" class="form-checkbox form-boolean form-boolean--type-checkbox testingValues" checked disabled>
+            `<div class="webform-flexbox js-webform-flexbox js-form-wrapper grow justify-between form-wrapper items-center" id="edit-flexbox-${test}"><div class="grow justify-start webform-flex"><div class="webform-flex--container"><div class="form-type-checkbox js-form-item form-item js-form-type-checkbox form-type--checkbox form-type--boolean js-form-item-test form-item--test">
+              <input data-drupal-selector="edit-${test}" type="checkbox" id="edit-${test}" name="${test}" value="${test}" class="form-checkbox form-boolean form-boolean--type-checkbox testingValues" ${testValue ? 'checked' : ''}>
               <span class="checkbox-toggle">
                 <span class="checkbox-toggle__inner"></span>
               </span>
@@ -81,18 +102,19 @@ function setTests() {
               </div>
             </div></div>
 
-            <div class="webform-flex flex-auto"><div class="webform-flex--container"><div class="form-type-textfield js-form-item form-item js-form-type-textfield form-type--textfield js-form-item-${test}-results form-item--${test}-results form-item--no-label">
+            <div id="container-${test}-results" class="webform-flex flex-auto ${!testValue ? 'hidden' : ''}"><div class="webform-flex--container"><div class="form-type-textfield js-form-item form-item js-form-type-textfield form-type--textfield js-form-item-${test}-results form-item--${test}-results form-item--no-label">
                 <label for="edit-${test}-results" class="form-item__label visually-hidden">test results</label>
-                <input data-drupal-selector="edit-${test}-results" type="text" id="edit-${test}-results" name="${test}_results" value="${testValue.testingResult}" size="60" maxlength="255" class="form-text form-element form-element--type-text form-element--api-textfield">
+                <input data-drupal-selector="edit-${test}-results" type="text" id="edit-${test}-results" name="${test}_results" value="${testValue?.testingResult ?? ''}" size="60" maxlength="255" class="form-text form-element form-element--type-text form-element--api-textfield">
             </div>
-            </div></div><div class="webform-flex flex-auto justify-end"><div class="webform-flex--container"><div class="webform-element--title-inline form-type-checkbox js-form-item form-item js-form-type-checkbox form-type--checkbox form-type--boolean js-form-item-passed form-item--passed">
-                <input data-drupal-selector="edit-${test}-passed" type="checkbox" id="edit-${test}-passed" name="${test}-passed" value="1" class="form-checkbox form-boolean form-boolean--type-checkbox" ${testValue.passed ? 'checked' : ''}>
+            </div></div><div id="container-${test}-passed" class="webform-flex flex-auto justify-end ${!testValue ? 'hidden' : ''}"><div class="webform-flex--container"><div class="webform-element--title-inline form-type-checkbox js-form-item form-item js-form-type-checkbox form-type--checkbox form-type--boolean js-form-item-passed form-item--passed">
+                <input data-drupal-selector="edit-${test}-passed" type="checkbox" id="edit-${test}-passed" name="${test}-passed" value="1" class="form-checkbox form-boolean form-boolean--type-checkbox" ${testValue?.passed ? 'checked' : ''}>
                 <span class="checkbox-toggle">
                     <span class="checkbox-toggle__inner"></span>
                 </span>
                 <label for="edit-${test}-passed" class="form-item__label option">Passed</label>
             </div>
             </div></div></div>`);
+
       } else {
         testing.append(`
           <div class="webform-element--title-inline form-type-checkbox js-form-item form-item js-form-type-checkbox form-type--checkbox form-type--boolean">
