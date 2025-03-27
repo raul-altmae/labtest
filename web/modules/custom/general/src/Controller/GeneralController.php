28adea54be94002e39c2f90e0d3c0bc1b45d70b3
Raul Altmäe <141125477+raul-altmae@users.noreply.github.com>
2025-03-27 18:38:20 +0200

    Refactor GeneralController: update terms and ignore keys, rename processTerms to processRows

--- web/modules/custom/general/src/Controller/GeneralController.php
+++ web/modules/custom/general/src/Controller/GeneralController.php
@@ -11,7 +11,9 @@ use Symfony\Component\HttpFoundation\JsonResponse;
  * Returns responses for general routes.
  */
 class GeneralController extends ControllerBase {
-  const SAMPLE_REQUEST_TERMS = ['country', 'customer', 'product', 'order_raised_by', 'sample_type', 'size_autocomplete', 'care_label', 'edge_finishing', 'stitch_pattern', 'needle_spacing', 'packaging', 'shipping_method', 'current_status', 'china'];
+  const TERMS = ['batch_no', 'component_1', 'component_2', 'component_3', 'component_4', 'component_5', 'material_1', 'material_2', 'material_3', 'material_4', 'material_5', 'product_type', 'reason_for_test', 'requested_by', 'service_required', 'testing_company'];
+  
+  const IGNORE_KEYS = ['test_result_1', 'test_result_2', 'test_result_3', 'test_result_4'];
 
   /**
    * Builds the response.
@@ -22,15 +24,19 @@ class GeneralController extends ControllerBase {
     /** @var \Drupal\webform\WebformSubmissionInterface $submission */
     foreach ($submissions as $submission) {
       $data = $submission->getData();
-      $this->processTerms($data);
+      $this->processRows($data);
+        
       $items[] = $data;
     }
     return JsonResponse::create(['items' => $items]);
   }
 
-  private function processTerms(array &$data) {
+  private function processRows(array &$data) {
     foreach ($data as $key => $value) {
-      if (in_array($key, self::SAMPLE_REQUEST_TERMS, TRUE)) {
+      if (in_array($key, self::IGNORE_KEYS, TRUE)) {
+        unset($data[$key]);
+      }
+      if (in_array($key, self::TERMS, TRUE)) {
         $data[$key] = $this->getTermName($value);
       }
     }
